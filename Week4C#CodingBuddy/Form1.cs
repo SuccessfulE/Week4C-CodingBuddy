869aaa38f4e1f2d827ab0ad24db4d4d49bb27c3f
Eduardo Rivera <Eduardo.Rivera@pgip-tech.com>
2023-11-10 17:45:49 -0500

    Changed some code

--- Week4C#CodingBuddy/Form1.cs
+++ Week4C#CodingBuddy/Form1.cs
@@ -30,26 +30,12 @@ namespace Week4C_CodingBuddy
                 Score++;
                 label1.Text = "" + Score;
 
-                //Moves road with speed set in sRoad
-                Water1.Left += waterSpeed;
-                Water2.Left += waterSpeed;
-
-                //police car 1 spawn
+                //plank spawn
                 plank1.Left += plankSpeed;
-                //police car random respawn
+                //plannk
                 if (plank1.Left > 691)
                 {
                     plank1.Left = rnd.Next(0, 420);
-                }
-
-                //Resetting roads
-                if (Water1.Left > 395)
-                {
-                    Water1.Left = -470;
-                }
-                if (Water2.Left > 395)
-                {
-                    Water2.Left = -470;
                 }
             }
         }
